afccc03e1a4489809f61bb4ab2b509372feafea2
Maros Kukan <maros.kukan@me.com>
2021-02-18 12:39:40 +0100

    Commented out testing script and added changed condition to email notification

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -15,14 +15,15 @@ pipeline {
             steps {
                 // Run Maven Wrapper
                 sh './mvnw clean package'
+                //sh 'false' // true
             }
           
             post {
                 always {
                     junit '**/target/surefire-reports/TEST-*.xml'
                     archiveArtifacts 'target/*.jar'
-            //   }
-            //   changed {
+              }
+              changed {
                 emailext subject: "Job \'${JOB_NAME}\' (${BUILD_NUMBER}) ${currentBuild.result}",
                     body: "Please go to ${BUILD_URL} and verify the build",
                     attachLog: true, 
